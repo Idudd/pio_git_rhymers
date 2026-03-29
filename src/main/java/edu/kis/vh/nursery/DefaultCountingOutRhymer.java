6e2da8e9a00097bfeb15265c71b06ab46944cd36
Andrii <258484@edu.p.lodz.pl>
2026-03-29 13:56:46 +0200

    10. Generowanie getter/setter dla pola total

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -9,6 +9,16 @@ public class DefaultCountingOutRhymer {
 
     private int total = EMPTY;
 
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+
+
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
