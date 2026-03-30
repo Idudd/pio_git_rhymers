8fad3cb1e3db701fad951f110d1c30350951d39f
Andrii Maslov <258484@edu.p.lodz.pl>
2026-03-30 11:24:11 +0200

    17. Dodanie i uzupełnienie dokumentacji JavaDoc dla RhymersDemo

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -7,7 +7,27 @@ import edu.kis.vh.nursery.factory.RhymersFactory;
 //Projekt jest poprawny
 
 
+/**
+ * Klasa demonstracyjna prezentująca działanie różnych implementacji
+ *  rymowanek (Rhymers).
+ *
+ *  Tworzy obiekty za pomocą fabryki {@link RhymersFactory}, a następnie
+ *  testuje ich zachowanie poprzez dodawanie i usuwanie elementów.
+ *
+ *  Program pokazuje różnice w działaniu różnych strategii:
+ *  standardowej, FIFO, fałszywej oraz Hanoi.
+ *
+ */
+
 class RhymersDemo {
+
+    /**
+     * Punkt wejścia programu.
+     *
+     * Tworzy fabrykę rymowanek i uruchamia test działania różnych implementacji.
+     *
+     * @param args argumenty linii poleceń (nieużywane)
+     */
 
     public static void main(String[] args) {
         RhymersFactory factory = new DefaultRhymersFactory();
@@ -15,6 +35,19 @@ class RhymersDemo {
         testRhymers(factory);
 
     }
+
+    /**
+     *  Metoda testująca różne implementacje rymowanek.
+     *
+     *  Tworzy tablicę obiektów typu {@code DefaultCountingOutRhymer}
+     *  przy użyciu fabryki, a następnie:
+     *      dodaje elementy do struktur danych,
+     *      losowo zasila jeden z obiektów,
+     *      opróżnia struktury i wypisuje wyniki,
+     *      raportuje liczbę odrzuconych elementów dla HanoiRhymer.
+     *
+     *  @param factory fabryka dostarczająca różne implementacje rymowanek
+     */
 
     private static void testRhymers(RhymersFactory factory) {
         DefaultCountingOutRhymer[] rhymers = { factory.GetStandardRhymer(), factory.GetFalseRhymer(),
